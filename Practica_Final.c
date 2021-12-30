67a6a9fac28f48af008fb84119821b111d31c458
Daniel-prog-web <daniel_tv97@hotmail.com>
2021-12-30 13:23:09 +0100

    megafuncion

--- Practica_Final.c
+++ Practica_Final.c
@@ -89,6 +89,37 @@ void tirolina(){
 
 }
 
+void MEGAFUNCION()
+{
+   case 1:
+      break;
+
+  case 2:
+      break;
+  case 3:
+        break;
+
+  case 4:
+        break;
+  case 5:
+          break;
+
+  case 6:
+      break;
+  case 7:
+        break;
+
+   case 8:
+        break;
+   case 9:
+          break;
+
+      case 10:
+          break;
+}
+
+
+
 void cascada{
 
 }
